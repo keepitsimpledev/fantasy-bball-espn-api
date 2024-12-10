6107e72807543d00bd82c7e036461a9f1f8d11e8
dev <keep.it.simple.dev@gmail.com>
2024-12-09 21:41:08 -0500

    format

--- test/test_espn_interactions.py
+++ test/test_espn_interactions.py
@@ -4,12 +4,21 @@ import unittest
 
 class TestBasketball(unittest.TestCase):
     def test_format_team_name(self):
-        self.assertEqual("The Basketballers", format_team_name(Team("The Basketballers", "")))
-        self.assertEqual("The Basketballers (TB)", format_team_name(Team("The Basketballers", "TB")))
-        self.assertEqual("TheBasketballers (TB)", format_team_name(Team("The⭐Basketballers", "TB")))
-        self.assertEqual("Who's a good boy (WHO)", format_team_name(Team("Who's a good boy?", "WHO")))
+        self.assertEqual(
+            "The Basketballers", format_team_name(Team("The Basketballers", ""))
+        )
+        self.assertEqual(
+            "The Basketballers (TB)", format_team_name(Team("The Basketballers", "TB"))
+        )
+        self.assertEqual(
+            "TheBasketballers (TB)", format_team_name(Team("The⭐Basketballers", "TB"))
+        )
+        self.assertEqual(
+            "Who's a good boy (WHO)", format_team_name(Team("Who's a good boy?", "WHO"))
+        )
 
-class Team():
+
+class Team:
     def __init__(self, name: str, abbrev: str):
         self.team_name = name
         self.team_abbrev = abbrev
