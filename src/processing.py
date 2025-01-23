a705899b4af7c3a09657d992c2a4d7303cedc6d4
dev <keep.it.simple.dev@gmail.com>
2025-01-22 21:57:29 -0500

    implement

---
+++ src/processing.py
@@ -0,0 +1,8 @@
+from src.constants import KEY_ROSTER, KEY_SCHEDULE
+
+def combine_rosters_and_schedules(rosters, schedules):
+    teams = {}
+    for team_name in rosters:
+        teams[team_name] = {KEY_ROSTER: rosters[team_name]}
+        teams[team_name][KEY_SCHEDULE] = schedules[team_name]
+    return teams
