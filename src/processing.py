ed0281e6fb8d3991b2085f5458d26d48ddc76a9d
dev <keep.it.simple.dev@gmail.com>
2025-01-29 23:52:28 -0500

    implement sorting and printing

--- src/processing.py
+++ src/processing.py
@@ -7,9 +7,12 @@ from src.caching import (
 from src.constants import (
     ESPN_LEAGUE_ID,
     YEAR,
+    KEY_STATS,
     KEY_ROSTER,
     KEY_SCHEDULE,
     LOAD_FROM_CACHE,
+    MY_TEAM,
+    NINE_CATEGORIES
 )
 from src.espn_interactions.basketball import (
     build_teamname_to_roster_map,
@@ -44,3 +47,38 @@ def construct_teams_and_stats_map():
     teams = combine_rosters_and_schedules(rosters, schedules)
 
     return [teams, players_stats_map]
+
+
+def construct_sorted_teams_stats_map(teams):
+    sorted_teams_stats = {}
+    for category in NINE_CATEGORIES:
+        sorted_teams_stats[category] = []
+
+    for team in teams:
+        for category in NINE_CATEGORIES:
+            category_count = len(sorted_teams_stats[category])
+            team_stat = teams[team][KEY_STATS][category]
+            stat_and_team = [team_stat, team]
+            if category_count == 0:
+                sorted_teams_stats[category].append(stat_and_team)
+            else:
+                for index in range(category_count):
+                    if team_stat > sorted_teams_stats[category][index][0]:
+                        sorted_teams_stats[category].insert(index, stat_and_team)
+                        break
+                    elif index + 1 == category_count:
+                        sorted_teams_stats[category].append(stat_and_team)
+    
+    return sorted_teams_stats
+
+
+def print_my_team_stats(teams):
+    sorted_teams_stats = construct_sorted_teams_stats_map(teams)
+    print(MY_TEAM + " stat rankings:")
+    for stat_category in NINE_CATEGORIES:
+        for rank in range(len(teams)):
+            if sorted_teams_stats[stat_category][rank][1] == MY_TEAM:
+                print("{} : {}".format(stat_category, rank + 1))
+                break
+            elif rank + 1 == len(teams):
+                print("not found: {} {}".format(MY_TEAM, stat_category))
--- test/test_processing.py
+++ test/test_processing.py
@@ -1,5 +1,8 @@
 from src.constants import KEY_ROSTER, KEY_SCHEDULE
-from src.processing import combine_rosters_and_schedules
+from src.processing import combine_rosters_and_schedules, construct_sorted_teams_stats_map, print_my_team_stats
+import src.processing as processing
+from io import StringIO 
+from unittest.mock import patch 
 
 import unittest
 
@@ -32,3 +35,80 @@ class TestProcessing(unittest.TestCase):
         self.assertEqual(teams[team0][KEY_SCHEDULE], [team1, team2])
         self.assertEqual(teams[team1][KEY_SCHEDULE], [team0, team2])
         self.assertEqual(teams[team2][KEY_SCHEDULE], [team0, team1])
+
+    def test_construct_sorted_teams_stats_map(self):
+        # arrange
+        processing.NINE_CATEGORIES = ["REB", "AST", "PTS"]  # reduce number of cats to simplify test
+        teamA = {
+            "stats": {
+                "REB": 7,
+                "AST": 5,
+                "PTS": 3
+            }
+        }
+        teamB = {
+            "stats": {
+                "REB": 5,
+                "AST": 8,
+                "PTS": 2
+            }
+        }
+        teamC = {
+            "stats": {
+                "REB": 6,
+                "AST": 5,
+                "PTS": 9
+            }
+        }
+        teams = {
+            "teamA": teamA,
+            "teamB": teamB,
+            "teamC": teamC,
+        }
+        
+        # act
+        sorted_team_stats = construct_sorted_teams_stats_map(teams)
+
+        # assert
+        self.assertEqual(sorted_team_stats["REB"], [[7, "teamA"], [6, "teamC"], [5, "teamB"]])
+        self.assertEqual(sorted_team_stats["AST"], [[8, "teamB"], [5, "teamA"], [5, "teamC"]])
+        self.assertEqual(sorted_team_stats["PTS"], [[9, "teamC"], [3, "teamA"], [2, "teamB"]])
+
+    def test_print_my_team_stats(self,):
+        # arrange
+        processing.NINE_CATEGORIES = ["REB", "AST", "PTS"]  # reduce number of cats to simplify test
+        processing.MY_TEAM = "teamB"
+        teamA = {
+            "stats": {
+                "REB": 7,
+                "AST": 5,
+                "PTS": 3
+            }
+        }
+        teamB = {
+            "stats": {
+                "REB": 5,
+                "AST": 8,
+                "PTS": 2
+            }
+        }
+        teamC = {
+            "stats": {
+                "REB": 6,
+                "AST": 5,
+                "PTS": 9
+            }
+        }
+        teams = {
+            "teamA": teamA,
+            "teamB": teamB,
+            "teamC": teamC,
+        }
+
+        with patch('sys.stdout', new = StringIO()) as captured_out: 
+            # act
+            print_my_team_stats(teams)
+
+            # assert
+            self.assertEqual(captured_out.getvalue(), "teamB stat rankings:\n"
+                             + "REB : 3\nAST : 1\nPTS : 3\n")
