43447e57de4d207b164e868f53c8f0be9bd348c2
dev <keep.it.simple.dev@gmail.com>
2025-11-16 22:42:21 -0500

    add and test compare_waiver_moves

--- src/calculations.py
+++ src/calculations.py
@@ -1,3 +1,4 @@
+import copy
 import logging
 from src.constants import (
     ALL_STATS,
@@ -10,6 +11,8 @@ from src.constants import (
     KEY_LOSSES,
     KEY_TIES,
 )
+from src.env import MY_TEAM
+from src.transactions import add, drop
 
 
 logger = logging.getLogger(__name__)
@@ -26,7 +29,7 @@ def calculate_team_stats(teams, players_stats_map):
                 logger.warning("stats not found for {}".format(player))
                 continue
             if KEY_IR not in players_stats_map[player]:
-                continue  # TODO check why this is necessary - what does a lack of an IR flag indicate?
+                raise Exception("TODO check why this is necessary - what does a lack of an IR flag indicate?")
             if players_stats_map[player][KEY_IR] == "True":
                 continue  # IR players won't contribute to calculations
             for stat in ALL_STATS:
@@ -55,3 +58,56 @@ def simulate_season(teams):
         teams[team_name][KEY_WINS] = wins
         teams[team_name][KEY_LOSSES] = losses
         teams[team_name][KEY_TIES] = ties
+
+
+def determine_worst_player():
+    print("unimplemeneted")
+
+
+def scored_transaction_comparator(st):
+    return -st.score
+
+
+class ScoredTransaction:
+    def __init__(self, drop, add, score):
+        self.drop = drop
+        self.add = add
+        self.score = score
+
+
+def compare_waiver_moves(teams, all_players_stats):
+    rostered_players = []
+    for team in teams:
+        for player in teams[team][KEY_ROSTER]:
+            rostered_players.append(player)
+
+    simulate_season(teams)
+    for team_to_calculate in teams:
+        if team_to_calculate == MY_TEAM:
+            original_win_count = teams[team_to_calculate][KEY_WINS]
+
+    scored_transactions = []
+    original_teams = copy.deepcopy(teams)
+    for team_to_calculate in teams:
+        if team_to_calculate == MY_TEAM:
+            for player_to_drop in teams[team_to_calculate][KEY_ROSTER]:
+                teams[team_to_calculate][KEY_ROSTER] = copy.deepcopy(original_teams[team_to_calculate][KEY_ROSTER])
+                drop(player_to_drop, teams)
+                roster_after_drop = copy.deepcopy(teams[team_to_calculate][KEY_ROSTER])
+                for fa_to_add in all_players_stats:
+                    if fa_to_add in rostered_players:
+                        continue
+                    teams[team_to_calculate][KEY_ROSTER] = copy.deepcopy(roster_after_drop)
+                    add(fa_to_add, team_to_calculate, all_players_stats, teams)
+                    calculate_team_stats(teams, all_players_stats)
+                    simulate_season(teams)
+                    for current_team in teams:
+                        if current_team == MY_TEAM:
+                            current_win_count = teams[current_team][KEY_WINS]
+                            scored_transactions.append(ScoredTransaction(player_to_drop, fa_to_add, current_win_count - original_win_count))
+
+    sorted_transactions = sorted(scored_transactions, key=scored_transaction_comparator)
+    num_to_reveal = 25 if len(sorted_transactions) > 25 else len(sorted_transactions)
+    for i in range(num_to_reveal):
+        t = sorted_transactions[i]
+        print("{} drop {} add {}".format(t.score, t.drop, t.add))
