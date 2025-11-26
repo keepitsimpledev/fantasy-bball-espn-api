373ddde6e50251e242b84f03e4172fc439f0c206
dev <keep.it.simple.dev@gmail.com>
2025-11-25 20:11:25 -0500

    some adjustments to main

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ from src.transactions import add, drop, trade
 
 
 def process_transactions(teams, players_stats_map):
+    print("\nprocessing transactions")  #TODO: add transaction processing logs
+
     drop("Keyonte George", teams)
     add("Mike Conley", MY_TEAM, players_stats_map, teams)
     trade(["James Harden", "Kelly Oubre Jr."], ["Stephen Curry", "Deni Avdija"], teams)
@@ -22,13 +24,19 @@ def process_transactions(teams, players_stats_map):
 
 if __name__ == "__main__":
     [teams, players_stats_map] = construct_teams_and_stats_map()
-    process_transactions(teams, players_stats_map)
+    
     calculate_team_stats(teams, players_stats_map)
-
     simulate_season(teams)
-    print_my_team_stat_rankings(teams)
     print_all_team_stats(teams)
+    
+    print_my_team_stat_rankings(teams)
+    
+    # process_transactions(teams, players_stats_map)
 
+    # calculate_team_stats(teams, players_stats_map)
+    # simulate_season(teams)
+    # print_all_team_stats(teams)
+    
     compare_waiver_moves(teams, players_stats_map)
     determine_worst_player(teams, players_stats_map)
 
